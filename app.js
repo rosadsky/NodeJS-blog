3e325a32b1a2d74884a25cbf5882a74bc16d8468
rosadsky <rosadsky@hotmail.com>
2021-06-05 22:32:15 +0200

    titles

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ app.get('/', (req, res) => {
 
     //res.send('<p> Home page </p>');
     //res.sendFile('./views/index.html',{ root: __dirname })
-    res.render('index');
+    res.render('index', { title: 'Home'});
     
 } );
 
 app.get('/about', (req, res) => {
 
-    res.render('about');
+    res.render('about', { title: 'About'});
 
 
    // res.send('<p> About page </p>');
@@ -34,18 +34,18 @@ app.get('/about', (req, res) => {
 
 app.get('/about-us', (req, res) => {
     //res.redirect('/about');
-    res.render('about');
+    res.render('about', { title: 'About'});
 })
-
+ 
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', { title: 'Create blog'});
 })
 
 //404 page
 
 app.use((req,res) => {
   //  res.status(404).sendFile('./views/404.html', {root: __dirname});
-    res.status(404).render('404');
+    res.status(404).render('404', { title: '404 page'});
     
 });
