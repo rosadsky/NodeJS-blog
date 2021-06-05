4bc6ca9b3b42baa24ac1f5938d75c33e94183000
rosadsky <rosadsky@hotmail.com>
2021-06-05 22:03:44 +0200

    commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+//express app
+
+const app = express();
+
+//listen fo request
+
+
+//register view engine
+
+app.set('view engine', 'ejs');
+
+
+
+app.listen(3000);
+
+app.get('/', (req, res) => {
+
+    //res.send('<p> Home page </p>');
+    //res.sendFile('./views/index.html',{ root: __dirname })
+    res.render('index');
+    
+} );
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+
+
+   // res.send('<p> About page </p>');
+    
+} );
+
+app.get('/about-us', (req, res) => {
+    //res.redirect('/about');
+    res.render('about');
+})
+
+
+//404 page
+
+app.use((req,res) => {
+  //  res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
+    
+});
